95b8858892327b378b57e22a904530cfb8718c1d
Yan Pujante <yan@pongasoft.com>
2024-06-18 10:45:45 -0700

    added device categories per spreadsheet

--- info.lua
+++ info.lua
@@ -15,6 +15,7 @@ product_id = "com.pongasoft.VAC6"
 manufacturer = "pongasoft"
 version_number = "1.0.1f13"
 device_type = "creative_fx"
+device_categories = {"Audio Meter"}
 supports_patches = false
 accepts_notes = false
 auto_create_track = false
